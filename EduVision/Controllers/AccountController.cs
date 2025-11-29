70d51037f991466236fee40fc7cfa5294e0e2715
Beyza Nur Birinci <beyzanurbirinci5@gmail.com>
2025-11-29 15:47:07 +0300

    Add project files.

---
+++ EduVision/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EduVision.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Welcome sayfanı döndür
+            return View("Welcome");
+        }
+        public IActionResult Welcome()
+        {
+            return View(); // Views/Account/Welcome.cshtml
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ EduVision/Controllers/QuestionBankController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EduVision.Controllers
+{
+    public class QuestionBankController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+
+            return View();
+        }
+    }
+}
